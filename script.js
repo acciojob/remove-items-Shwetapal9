1573a55dcdd7ef8cbec7ece753f1d58adb71078e
Shweta pal <51901918+Shwetapal9@users.noreply.github.com>
2023-05-26 18:37:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function deleteElements(){
+	let selectedElement = document.getElementById("colorSelect");
+	let selectedValue = selectedElement.value;
+	let childrenList = selectedElement.children;
+	let retainedElements = [];
+	 for(let i=0 ;i< childrenList.length ; i++)
+		 {
+			 if(selectedValue===childrenList[i].value)
+			 {
+				 retainedElements.push(selectedValue)
+				 childrenList[i].remove();
+			 }
+				 
+		 }
+}
